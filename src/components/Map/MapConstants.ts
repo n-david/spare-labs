42ec4e98d1e8f999d52f706e8b9bd4ae6db572b0
n-david <ngdavid.93@gmail.com>
2018-11-06 01:37:48 -0800

    prod env proxy, add favicon.ico

--- src/components/Map/MapConstants.ts
+++ src/components/Map/MapConstants.ts
@@ -1,9 +1,16 @@
+function getURLPrefix() {
+    if (process.env.NODE_ENV === 'production') {
+        return 'api';
+    }
+    return '';
+}
+
 export const MAPBOX_STYLE = 'mapbox://styles/n-david/cjo43qjnh6gqx2rp6tal853tb';
 export const TRANSLINK_API = (
     latitude: number,
     longitude: number,
     radius: number,
 ) =>
-    `${process.env.production ? 'api' : ''}/RTTIAPI/V1/stops?apiKey=${
+    `${getURLPrefix()}/RTTIAPI/V1/stops?apiKey=${
         process.env.REACT_APP_TRANSLINK_TOKEN
     }&lat=${latitude}&long=${longitude}&Radius=${radius}`;
