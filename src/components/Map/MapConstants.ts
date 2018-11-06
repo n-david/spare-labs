92df821aceccb8942a0953818a67b9a2bce0d7d9
n-david <ngdavid.93@gmail.com>
2018-11-06 01:45:52 -0800

    Final prod env proxy fix

--- src/components/Map/MapConstants.ts
+++ src/components/Map/MapConstants.ts
@@ -1,16 +1,9 @@
-function getURLPrefix() {
-    if (process.env.NODE_ENV === 'production') {
-        return 'api';
-    }
-    return '';
-}
-
 export const MAPBOX_STYLE = 'mapbox://styles/n-david/cjo43qjnh6gqx2rp6tal853tb';
 export const TRANSLINK_API = (
     latitude: number,
     longitude: number,
     radius: number,
 ) =>
-    `${getURLPrefix()}/RTTIAPI/V1/stops?apiKey=${
+    `api/RTTIAPI/V1/stops?apiKey=${
         process.env.REACT_APP_TRANSLINK_TOKEN
     }&lat=${latitude}&long=${longitude}&Radius=${radius}`;
